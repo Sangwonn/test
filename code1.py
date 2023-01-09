efa912f4a91aa5f17c8a11ba33ef29c64d89180a
Sangwon <kimssw0202@naver.com>
2023-01-09 16:37:26 +0900

    code2, code3 제거 후 code1 병합

--- code1.py
+++ code1.py
@@ -4,9 +4,54 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
+from sklearn import model_selection
+
 Iris = load_iris()
 Iris_Data = pd.DataFrame(data=np.c_[Iris['data'], Iris['target']], columns=Iris['feature_names'] + ['target'])
 Iris_Data['target'] = Iris_Data['target'].map({0: "setosa", 1: "versicolor", 2: "virginica"})
 
 X_Data = Iris_Data.iloc[:, :-1]
 Y_Data = Iris_Data.iloc[:, [-1]]
+
+models =[]
+models.append(("LR", LogisticRegression()))
+models.append(("DT", DecisionTreeClassifier()))
+models.append(("SVM", SVC()))
+models.append(("NB", GaussianNB()))
+models.append(("KNN", KNeighborsClassifier()))
+models.append(("RF", RandomForestClassifier()))
+models.append(("GB", GradientBoostingClassifier()))
+models.append(("ANN", MLPClassifier()))
+
+for name, model in models:
+    model.fit(X_Data, Y_Data.values.ravel())
+    Y_pred = model.predict(X_Data)
+    print(name, "'s Accuracy is ", accuracy_score(Y_Data, Y_pred))
+
+results = []
+names = []
+
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=5,
+                                  random_state=7, shuffle=True)
+    cv_results = model_selection.cross_val_score(
+        model, X_Data, Y_Data.values.ravel(), cv=kfold, scoring='accuracy'
+    )
+    results.append(cv_results)
+    names.append(name)
+fig = plt.figure()
+fig.suptitle('Classifier Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
